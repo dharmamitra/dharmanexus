75e8db6429f5df47038038b13e3f4c4107477a2b
aminahbl <aminahbl.github@37e.uk>
2024-10-30 11:22:37 +0000

    create mitra suspended pages dir

--- frontend/src/mitra/history/update_history.sh
+++ frontend/src/mitra/history/update_history.sh
@@ -8,9 +8,7 @@ TEMP_FILE="${OUTPUT_DIR}/temp.txt"
 
 # List staged or changed files excluding src/mitra/
 git status --porcelain | grep -v "^??" | grep -P -v "src/mitra/" >"${TEMP_FILE}"
+sort -u "${TEMP_FILE}" -o "${TEMP_FILE}"
 grep -P "^\s?D" "${TEMP_FILE}" | sed -r 's/^\s?D //' >>"${DELETED_FILE}"
 grep -P "^\s?A" "${TEMP_FILE}" | sed -r 's/^\s?A //' >>"${ADDED_FILE}"
 grep -P "^\s?M" "${TEMP_FILE}" | sed -r 's/^\s?M //' >>"${ADJUSTED_FILE}"
-sort -u "${DELETED_FILE}" -o "${DELETED_FILE}"
-sort -u "${ADDED_FILE}" -o "${ADDED_FILE}"
-sort -u "${ADJUSTED_FILE}" -o "${ADJUSTED_FILE}"
