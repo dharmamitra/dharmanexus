1fd80e99999283ffae9b6a57d4ae565dd229fe14
aminahbl <aminahbl.github@37e.uk>
2025-06-23 00:55:27 +0200

    fix missing backgroundName var

--- frontend/src/components/db/ErrorPage.tsx
+++ frontend/src/components/db/ErrorPage.tsx
@@ -1,17 +1,12 @@
 import { useTranslation } from "next-i18next";
-import type { BackgroundName } from "@components/layout/PageContainer";
 import { PageContainer } from "@components/layout/PageContainer";
 import { Typography } from "@mui/material";
 
-export function ErrorPage({
-  backgroundName,
-}: {
-  backgroundName: BackgroundName;
-}) {
+export function ErrorPage() {
   const { t } = useTranslation();
 
   return (
-    <PageContainer backgroundName={backgroundName}>
+    <PageContainer>
       <Typography variant="h1">Error</Typography>
       <Typography variant="h3" sx={{ py: 2 }}>
         {t("prompts.genericErrorTitle")}
